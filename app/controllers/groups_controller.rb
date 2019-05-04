40360ed821e18d3de4a0bca18c84cfbadf582259
shinya8818 <wacker8818@outlook.com>
2019-05-04 11:48:49 +0900

    update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
 
   def index
   end
-
+  
   def new
     @group = Group.new
     @group.users << current_user
@@ -20,7 +20,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: "グループを編集しました"
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
     end
@@ -29,9 +29,9 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, { user_ids: []})
   end
-  
+
   def set_group
     @group = Group.find(params[:id])
   end
