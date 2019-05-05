e4036de44a12d81661ea790ea8340c586afc5dbb
shinya8818 <wacker8818@outlook.com>
2019-05-05 11:14:30 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,19 +2,19 @@ $(function(){
   function buildHTML(message){
     var image_url = (message.image_url)? `<image class="lower-message_image" src="${message.image_url}">`:"";
     var html = `<div class="message" id='${message.id}'>
-                  <div class="upper-message" >
-                    <div class="upper-message__name">
-                    ${message.name}
-                    </div>
-                    <div class="upper-message__time">
-                    ${message.time}
-                  </div>
-                </div>
-                  <div class="lower-message">
-                    <p class="lower-message__content"></p>
-                    ${message.content}
-                    ${image_url}
-                  </div>`
+      <div class="upper-message" >
+        <div class="upper-message__name">
+          ${message.name}
+      </div>
+        <div class="upper-message__time">
+          ${message.time}
+      </div>
+    </div>
+      <div class="lower-message">
+        <p class="lower-message__content"></p>
+          ${message.content}
+          ${image_url}
+      </div>`
     return html;
   }
   $('#new_message').on('submit',function(e){
