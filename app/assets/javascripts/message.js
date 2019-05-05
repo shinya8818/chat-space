38f5141b35f5b055f8e749d3629a8601ce6cff6c
shinya8818 <wacker8818@outlook.com>
2019-05-05 13:38:29 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -17,6 +17,9 @@ $(function(){
       </div>`
     return html;
   }
+  function always(){
+    $('.form__submit').prop('disabled',false)
+  }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -34,12 +37,12 @@ $(function(){
       $('.messages').append(html);
       $('.form__message').val('');
       $('.hidden').val('');
-      $('.form__submit').prop('disabled',false);
+      always()
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight},'fast');
     })
     .fail(function(data){
       alert('error');
-      $('.form__submit').prop('disabled',false)
+      always()
     })
   });
 });
