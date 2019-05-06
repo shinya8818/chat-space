dd4c4e6291fb95b0c7e46f4bfa30e51e9c6b6530
shinya8818 <wacker8818@outlook.com>
2019-05-06 09:55:35 +0900

    create

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,25 @@
+$(function() {
+  $("#user-search-field").on("keyup",function(e) {
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input},
+      dataType: 'json'
+    })
+    .done(function(users) {
+      if(users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーはいません")
+      }
+    })
+    // .fail(function() {
+    //   alert('error');
+    // });
+  });
+});
