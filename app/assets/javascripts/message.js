c8be20d22b121918642276f9a403f20c99e1ba62
shinya8818 <wacker8818@outlook.com>
2019-05-08 16:20:36 +0900

    updata

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,6 +36,7 @@ $(document).on('turbolinks:load', function(){
       .done(function(data){
         var html = buildHTML(data);
         $('.messages').append(html);
+        $('#new_message')[0].reset();
         $('hidden').val('');
         always()
         $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight},'fast');
