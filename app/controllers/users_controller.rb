4f41ed052f9b4d35acba873e549f99db4ace448a
shinya8818 <wacker8818@outlook.com>
2019-05-08 09:18:28 +0900

    インクリメンタル

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@
 class UsersController < ApplicationController
 
   def index
-    ＠users ＝ User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?) AND id !=(?)', "%#{params[:keyword]}%",current_user.id)
     respond_to do |format|
       format.html
       format.json
     end
-  end
+   end
 
   def edit
   end
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+  
 
   private
 
