e68f5d5ee0778c98dc64cd28daa5b01881602e67
shinya8818 <wacker8818@outlook.com>
2019-05-11 10:41:21 +0900

    up

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
-
+set :linked_files, %w{ config/secrets.yml }
 set :application, "chat-space"
 set :repo_url, 'git@github.com:shinya8818/chat-space.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -14,13 +14,6 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     invoke 'unicorn:restart'
-#   end
-# end
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
@@ -78,5 +71,3 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
-
-set :linked_files, %w{ config/secrets.yml }
